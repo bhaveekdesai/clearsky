db50e720c29056f890d636566f048b5b27c13082
Bhaveek Desai <bhaveekdesai@gmail.com>
2018-01-31 13:55:23 -0500

    chore(module-rest): add properties file and make controller fetch values from it

--- rest/clearsky/src/main/java/io/bhaveekdesai/api/JPAConfig.java
+++ rest/clearsky/src/main/java/io/bhaveekdesai/api/JPAConfig.java
@@ -1,8 +1,11 @@
 package io.bhaveekdesai.api;
 
 import io.bhaveekdesai.api.constants.URI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -16,7 +19,11 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource(value="classpath:application.properties")
 public class JPAConfig {
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean emf(){
@@ -32,9 +39,9 @@ public class JPAConfig {
     public DataSource getDataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName(URI.DRIVER_CLASS_NAME);
-        ds.setUrl(URI.MYSQL_URL);
-        ds.setUsername(URI.MYSQL_USERNAME);
-        ds.setPassword(URI.MYSQL_PASSWORD);
+        ds.setUrl(env.getProperty("db.url"));
+        ds.setUsername(env.getProperty("db.user"));
+        ds.setPassword(env.getProperty("db.password"));
         return ds;
     }
 
@@ -46,9 +53,9 @@ public class JPAConfig {
     private Properties jpaProperties() {
         Properties props = new Properties();
         props.setProperty(URI.PROP_HIBERNATE_DIALECT, URI.PROP_HIBERNATE_DIALECT_VALUE);
-        props.setProperty(URI.PROP_HIBERNATE_HBM2DDL_AUTO, URI.PROP_HIBERNATE_HBM2DDL_AUTO_VALIDATE);
-        props.setProperty(URI.PROP_HIBERNATE_SHOW_SQL,"true");
-        props.setProperty(URI.PROP_HIBERNTE_FORMAT_SQL,"true");
+        props.setProperty(URI.PROP_HIBERNATE_HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl"));
+        props.setProperty(URI.PROP_HIBERNATE_SHOW_SQL,env.getProperty("hibernate.show.sql","true"));
+        props.setProperty(URI.PROP_HIBERNTE_FORMAT_SQL,env.getProperty("hibernate.format.sql","true"));
         return props;
     }
 }
--- rest/clearsky/src/main/java/io/bhaveekdesai/api/constants/URI.java
+++ rest/clearsky/src/main/java/io/bhaveekdesai/api/constants/URI.java
@@ -13,25 +13,16 @@ public class URI {
     public static final String PRESSURE = "pressure";
     public static final String TEMPERATURE = "temperature";
     public static final String WIND = "wind";
-    public static final String WIND_SPEED= "wind_speed";
-    public static final String WIND_DEGREE= "wind_degree";
-
 
     //JPA Config
     public static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
-    public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/weather-db";
-    public static final String MYSQL_USERNAME = "root";
-    public static final String MYSQL_PASSWORD = "password";
     public static final String PROP_HIBERNATE_DIALECT = "hibernate.dialect";
     public static final String PROP_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
     public static final String PROP_HIBERNATE_SHOW_SQL = "hibernate.show.sql";
     public static final String PROP_HIBERNTE_FORMAT_SQL = "hibernate.format.sql";
 
     public static final String PROP_HIBERNATE_DIALECT_VALUE = "org.hibernate.dialect.MySQLDialect";
-    public static final String PROP_HIBERNATE_HBM2DDL_AUTO_CREATE = "create";
-    public static final String PROP_HIBERNATE_HBM2DDL_AUTO_VALIDATE = "validate";
 
     //public static final String  = "";
-
 
 }
